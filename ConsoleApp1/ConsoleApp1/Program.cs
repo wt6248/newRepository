1c676db8aa640c2cb3c38de9e8f0b58b9ccee52e
Song Si Ho <siho8539@postech.ac.kr>
2018-10-12 19:46:09 +0900

    breave 잭ㅇ aksema.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell World");
+            Console.WriteLine("breave World");
         }
     }
 }
